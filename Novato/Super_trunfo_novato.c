5166de07b4fb7c37186f937120e84b63fa1373c7
Wagner Neves <wagner.nevez7@gmail.com>
2026-02-17 02:50:41 +0000

    começando o projeto

---
+++ Novato/Super_trunfo_novato.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+char Carta1[10];
+char Estado[50];
+char Código[20];
+char cidade[20];
+int População, Pontos turísticos;
+float Área, PIB;}
+
+
+
+
+
+
+
+
+
+
+int main (){
+char Carta2[10];
+char Estado[50];
+char Código[20];
+char cidade[20];
+int População, Pontos turísticos;
+float Área, PIB;}

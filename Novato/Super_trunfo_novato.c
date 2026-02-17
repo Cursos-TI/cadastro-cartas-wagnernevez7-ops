948a52cea20d83eca3dcf42cf6c013deb0d8defb
Wagner Neves <wagner.nevez7@gmail.com>
2026-02-17 21:13:35 +0000

    Codando

--- Novato/Super_trunfo_novato.c
+++ Novato/Super_trunfo_novato.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
 int main (){
-char Carta1[10] = "A01";
-char Estado[50]; = "Minas gerais";
-char Código[20];
-char cidade[20];
-int População, Pontos turísticos;
-float Área, PIB;}
+char Carta1[10] = "Rara";
+char Estado[50] = "Minas gerais";
+char Código[20] = "A01";
+char cidade[20] = "Belo horizonte";
+int População = "100.000";
+int Pontos_turísticos = "20";
+float Área = (259.000);
+float PIB = (400.000);
+
+printf("Carta: %s\n Estado: %s\n Código: %s\n Cidade: %s\n População: %d\n Pontos turísticos: %d\n Área: %f\n PIB: %f\n", Carta1, Estado, Código, cidade, População, Pontos_turísticos, Área, PIB);
 
 
+char Carta2[10] = "Lendária";
+char Estado[50] = "São paulo";
+char Código[20] = "B01";
+char cidade[20] = "Guarulhos";
+int População = 1000000;
+int Pontos_turísticos = 89;
+float Área = 128.000;
+float PIB = 950.000;
 
+printf("Carta: %s\n Estado: %s\n Código: %s\n Cidade: %s\n População: %d\n Pontos turísticos: %d\n Área: %f\n PIB: %f\n", Carta2, Estado, Código, cidade, População, Pontos_turísticos, Área, PIB);
 
+return 0;}
 
-
-
-
-
-
-int main (){
-char Carta2[10];
-char Estado[50];
-char Código[20];
-char cidade[20];
-int População, Pontos turísticos;
-float Área, PIB;}
